617d87224946346ba852aa1bee95e911863e3c77
Hassan Barati <48393874+hsnpuply@users.noreply.github.com>
2025-01-23 10:05:26 +0330

    sliming side bar

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,23 +1,35 @@
 <script setup>
 import { ref } from "vue";
+const isSlim_sidebar = ref(false)
 
 const dashboard_activation = ref(false);
 const toggle_dashoard = () => {
   dashboard_activation.value = !dashboard_activation.value;
 };
 
+const toggle_sidebar = ()=>{
+  isSlim_sidebar.value = !isSlim_sidebar.value
+}
+
 import { Icon } from "@iconify/vue";
 </script>
 <template>
   <div class="main_content bg-[##fefefe] bg-slate-400 w-full min-h-[100vh] flex items-start">
-    <div class="sidebar min-w-[17.5rem] bg-white min-h-[100vh] text-menu_gray relative border-r-2 border-l-gray_border_right">
-      <div class="absolute  border-gray_border_right border-2  group right-[-6%] top-5 bg-white w-[1.875rem] h-[1.875rem] z-[99] rounded-[.5rem] flex items-center justify-center text-xl">
-        <Icon icon="line-md:arrow-close-left" width="14" height="14" class="text-gray_500" />
+    <div class="sidebar  bg-white min-h-[100vh] w-fit
+     text-menu_gray relative border-r-2 border-l-gray_border_right duration-500 transition-all"
+      :class="isSlim_sidebar ? '!max-w-[4.5rem] hover:!min-w-[17.5rem]' : 'min-w-[17.5rem]'"
+     >
+      <div class="absolute  border-gray_border_right border-2 
+       group right-[-6%] top-5 bg-white w-[1.875rem] duration-300 h-[1.875rem] z-[99] rounded-[.5rem] cursor-pointer
+       flex items-center justify-center text-xl"
+       @click="toggle_sidebar"
+       >
+        <Icon icon="line-md:arrow-close-left" width="14" height="14" :class="{'rotate-180 ' : isSlim_sidebar }" class="duration-500 text-gray_500 hover:text-gray_badge_txt" />
         </div>
       <div class="logo p-[1.3rem] mb-8">
-        <img src="../../assets/src/default-logo.svg" alt="" />
+        <img src="../../assets/src/default-logo.svg" alt="" :class="isSlim_sidebar ? 'w-full object-contain' : ''" />
       </div>
-      <div class="dashboard_nav pl-8 flex flex-col items-start ">
+      <div class="dashboard_nav pl-8 flex flex-col items-start overflow-hidden">
         <div
           class="dashboard flex flex-col gap-[0.75rem] w-full cursor-pointer"
         >
@@ -28,12 +40,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:dashboard-square-01"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 w-[1.25rem] h-[1.25rem]"
 
               />
-              <span class="ml-4">Dashboard</span>
+              <span class="ml-4 " :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">Dashboard</span>
             </div>
             <Icon :icon="dashboard_activation ? 'hugeicons:minus-sign' : 'hugeicons:plus-sign'" class="text-gray_500" width="18" height="18"  />
           </div>
@@ -51,14 +62,14 @@ import { Icon } from "@iconify/vue";
         </div>
       </div>
 
-      <div class="all_sections flex flex-col items-start gap-4 w-full ">
+      <div class="all_sections flex flex-col items-start gap-4 w-full  overflow-hidden">
 
       <div class="user_section pl-8 flex flex-col items-start  w-full    mt-3">
-        <h3 class="uppercase text-gray_500 mb-[1.375rem]">user</h3>
+        <h3 class="uppercase text-gray_500 mb-[1.375rem]">{{ isSlim_sidebar ? '...' : 'User' }}</h3>
       <!-- user section -->
-        <div class="users_nav flex flex-col gap-[1.463rem] w-full   ">
+        <div class="users_nav flex flex-col justify-start gap-[1.463rem] w-full   ">
           <div
-          class="dashboard flex flex-col gap-[0.75rem] w-full cursor-pointer"
+          class="dashboard flex flex-col  gap-[0.75rem] w-full cursor-pointer"
         >
           <div
             class="head_menu flex items-center justify-between mr-4"
@@ -67,13 +78,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center justify-between ">
               <Icon
                 icon="hugeicons:user-circle-02"
-                width="18"
-                class="text-gray_500"
-                height="18"
+                class="text-gray_500 w-[2.25rem] h-[2.25rem]"
               />
-              <span class="ml-4">Public Profile</span>
+              <span class="ml-4 " :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">Public Profile</span>
             </div>
-            <Icon icon="hugeicons:plus-sign" class="text-gray_500" width="18" height="18" />
+            <Icon icon="hugeicons:plus-sign" class="text-gray_500 w-[1.25rem] h-[1.25rem]"  />
           </div>
 
         </div>
@@ -88,12 +97,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:setting-07"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 !w-[1.75rem] h-full"
 
               />
-              <span class="ml-4 ">My Account</span>
+              <span class="ml-4 " :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">My Account</span>
             </div>
             <Icon icon="hugeicons:plus-sign" class="text-gray_500" width="18" height="18" 
             
@@ -113,12 +121,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:user-multiple-02"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 w-[1.25rem] h-[1.25rem]"
 
               />
-              <span class="ml-4">Network</span>
+              <span class="ml-4 " :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">Network</span>
             </div>
             <Icon icon="hugeicons:plus-sign" class="text-gray_500" width="18" height="18" />
           </div>
@@ -137,12 +144,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:location-user-04"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 w-[1.25rem] h-[1.25rem]"
 
               />
-              <span class="ml-4">Authentication</span>
+              <span class="ml-4 " :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">Authentication</span>
             </div>
             <Icon icon="hugeicons:plus-sign" class="text-gray_500" width="18" height="18" />
           </div>
@@ -154,7 +160,7 @@ import { Icon } from "@iconify/vue";
 
       <!-- APPS SEction -->
       <div class="user_section pl-8 flex flex-col items-start   mt-3">
-        <h3 class="uppercase text-gray_500 mb-[1.375rem]">Apps</h3>
+        <h3 class="uppercase text-gray_500 mb-[1.375rem]">{{ isSlim_sidebar ? '...' : 'Apps' }}</h3>
       <!-- Apps section -->
         <div class="apps_nav flex flex-col gap-[1.463rem]">
           <div
@@ -167,12 +173,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:user-multiple-02"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 w-[1.25rem] h-[1.25rem]"
 
               />
-              <span class="ml-4 not-active">User Managment</span>
+              <span class="ml-4 not-active" :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">User Managment</span>
             </div>
             <div class="bg-gray_badge_bg text-gray_badge_txt rounded-lg p-1 text-[0.625rem]"><p>Soon</p></div>
           </div>
@@ -189,12 +194,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:scroll"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 w-[1.25rem] h-[1.25rem]"
 
               />
-              <span class="ml-4 not-active">Projects</span>
+              <span class="ml-4 not-active" :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">Projects</span>
             </div>
             <div class="bg-gray_badge_bg text-gray_badge_txt rounded-lg p-1 text-[0.625rem]"><p>Soon</p></div>
           </div>
@@ -212,12 +216,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:shopping-cart-01"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 w-[1.25rem] h-[1.25rem]"
 
               />
-              <span class="ml-4 not-active">eCommerce</span>
+              <span class="ml-4 not-active" :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">eCommerce</span>
             </div>
             <div class="bg-gray_badge_bg text-gray_badge_txt rounded-lg p-1 text-[0.625rem]"><p>Soon</p></div>
           </div>
@@ -232,7 +235,7 @@ import { Icon } from "@iconify/vue";
 
       <!-- Miscellaneous -->
       <div class="miscellaneous_section pl-8 flex flex-col items-start w-full   mt-3">
-        <h3 class="uppercase text-gray_500 mb-[1.375rem] text-[13px ] ">Miscellaneous</h3>
+        <h3 class="uppercase text-gray_500 mb-[1.375rem] text-[13px ] " >{{ isSlim_sidebar ? '...' : 'Miscellaneous' }}</h3>
       <!-- Miscellaneous section -->
         <div class="users_nav flex flex-col gap-[1.463rem]  w-full">
           <div
@@ -245,12 +248,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:ai-network"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 w-[1.25rem] h-[1.25rem]"
 
               />
-              <span class="ml-4 not-active">Modals</span>
+              <span class="ml-4 not-active" :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">Modals</span>
             </div>
             <div class="bg-gray_badge_bg text-gray_badge_txt rounded-lg p-1 text-[0.625rem]"><p>Soon</p></div>
           </div>
@@ -267,12 +269,11 @@ import { Icon } from "@iconify/vue";
             <div class="dashboard flex items-center">
               <Icon
                 icon="hugeicons:magic-wand-01"
-                width="18"
-                height="18"
-                class="text-gray_500"
+                
+                class="text-gray_500 w-[1.25rem] h-[1.25rem]"
 
               />
-              <span class="ml-4 not-active">Wizards</span>
+              <span class="ml-4 not-active" :class="isSlim_sidebar ? 'opacity-0' : 'opacity-100'">Wizards</span>
             </div>
             <div class="bg-gray_badge_bg text-gray_badge_txt rounded-lg p-1 text-[0.625rem]"><p>Soon</p></div>
           </div>
